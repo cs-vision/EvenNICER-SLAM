1102266111de0e3f82928420c15c48ff7f2aadb2
Maemaemaeko <yamaguchi-mae512@g.ecc.u-tokyo.ac.jp>
2023-07-05 23:37:13 +0200

    fix matrix multiplication

--- temporary/matrix_mulplication.py
+++ temporary/matrix_mulplication.py
@@ -1,19 +1,25 @@
 import numpy as np
 import torch
+from src.common import get_camera_from_tensor
+
+tensor = torch.tensor([[0, 0, 0, 0.94, -2.5, -0.3, 0.45]]).to("cuda:0")
+matrix1 = get_camera_from_tensor(tensor).cpu()
 
 
-matrix1 = np.array([[-3.2057e-01, -4.4806e-01,  8.3455e-01,  3.4530e+00],
-        [ 9.4722e-01, -1.5164e-01,  2.8244e-01,  4.5461e-01],
-        [ 1.0790e-16,  8.8105e-01,  4.7302e-01,  5.9363e-01],
-        [0,0,0,1]])
-# matrix2 = np.array([[ 9.9966e-01, -2.6146e-02, -3.5900e-04, -6.7127e-04],
-#         [ 2.6141e-02,  9.9961e-01, -1.0219e-02, -5.0680e-04],
-#         [ 6.2605e-04,  1.0206e-02,  9.9995e-01,  5.2363e-04]])
+# matrix1 = np.array([[-3.2057e-01, -4.4806e-01,  8.3455e-01,  3.4530e+00],
+#         [ 9.4722e-01, -1.5164e-01,  2.8244e-01,  4.5461e-01],
+#         [ 1.0790e-16,  8.8105e-01,  4.7302e-01,  5.9363e-01],
+#         [0,0,0,1]])
+matrix2 = np.array([[ 0.9995, -0.0243, 0.0207, -0.0013],
+        [ 0.0245,  0.9996, -0.0104, -5.0680e-04],
+        [ -0.02,  0.0109,  9.9995e-01,  -0.0044]])
 
-matrix2 = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,1]]) #(3,4)
+#matrix2 = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,1]]) #(3,4)
 matrix2 = torch.tensor(matrix2, dtype=torch.float)
 
 bottom = torch.from_numpy(np.array([0, 0, 0, 1.]).reshape([1, 4])).type(torch.float32)
+print(matrix1.shape)
+#matrix1 = torch.cat([matrix1, bottom], dim=0)
 matrix2 = torch.cat([matrix2, bottom], dim=0)
 
 matrix1 = torch.tensor(matrix1, dtype=torch.float)  # (3,4)
@@ -22,7 +28,7 @@ matrix1 = torch.tensor(matrix1, dtype=torch.float)  # (3,4)
 
 result = torch.matmul(matrix1,matrix2)[:3, :] #(4,4)*(4,3)=(4,3)
 print(result)
-result = torch.matmul(matrix2,matrix1.t()) #(4,4)*(4,3)=(4,3)
+#result = torch.matmul(matrix2,matrix1.t()) #(4,4)*(4,3)=(4,3)
 
 #print(result)
 
